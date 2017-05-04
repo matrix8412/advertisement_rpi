d13b8d6141484b4204d0b65e59180fd47c398b58
matrix8412 <susek.martin@gmail.com>
2017-05-04 11:03:07 +0200

    Update video_loop_watchdog.php

--- opt/scripts/video_loop_watchdog.php
+++ opt/scripts/video_loop_watchdog.php
@@ -2,10 +2,10 @@
 $pid = exec("ps axf |grep \"sudo /usr/local/bin/video_loop\" | grep -v grep | awk '{print $1}'");
 
 if($pid != " " && $pid != ""){
+  echo "Video loop running.";
+}else{
   exec("sudo /usr/local/bin/video_loop > /dev/null 2>&1 &");
   echo "Player not running. Starting Video loop...";
-}else{
-  echo "Video loop running.";
 }
 
 
