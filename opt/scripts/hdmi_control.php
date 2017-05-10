c680fce8cdcb5d663424896b499a3ecab5b4cc0c
matrix8412 <susek.martin@gmail.com>
2017-05-10 15:14:58 +0200

    Update hdmi_control.php

--- opt/scripts/hdmi_control.php
+++ opt/scripts/hdmi_control.php
@@ -4,15 +4,13 @@ include("/opt/cron/config.php");
 $sn = exec("cat /proc/cpuinfo | grep Serial | awk '{print $3}'");
 
 $current_time = date("H:i");
-$hdmi_control = explode("-", exec("curl -sk ".$cloud_hostname."/api/api.php --data \"action=get_hdmi_control&sn=$sn\""));
+$hdmi_control_result = exec("curl -sk ".$cloud_hostname."/api/api.php --data \"action=get_hdmi_control&sn=$sn\"");
+$hdmi_control = explode("-", $hdmi_control_result);
 
-$date1 = DateTime::createFromFormat('H:i', $current_time);
-$date2 = DateTime::createFromFormat('H:i', $hdmi_control[1]);
-$date3 = DateTime::createFromFormat('H:i', $hdmi_control[2]);
+$current_time = date("H:i:s");
 
-
-if($date1 > $date2 && $date1 < $date3){
-  exec("/opt/vc/bin/tvservice -M");
+if($current_time > $hdmi_control[1] && $current_time < $hdmi_control[2]){
+  exec("/opt/vc/bin/tvservice -p");
   echo "Turning on HDMI...\n";
 }else{
   exec("/opt/vc/bin/tvservice -o");
@@ -20,3 +18,4 @@ if($date1 > $date2 && $date1 < $date3){
 }
 
 ?>
+
