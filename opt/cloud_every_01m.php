5a36b8b02bb7f16a7cdb6b7b419f1a45409d8492
matrix8412 <susek.martin@gmail.com>
2017-05-10 14:50:02 +0200

    Update cloud_every_01m.php

--- opt/cloud_every_01m.php
+++ opt/cloud_every_01m.php
@@ -11,7 +11,7 @@ exec("sudo mkdir -p /run/advrpi_files/xml");
 
 exec("sudo php /opt/cron/scripts/device_stats.php");
 exec("sudo php /opt/cron/scripts/video_loop_watchdog.php");
-#exec("sudo php /opt/cron/scripts/network_stats.php");
+exec("sudo php /opt/cron/scripts/hdmi_control.php");
 
 ?>
 
