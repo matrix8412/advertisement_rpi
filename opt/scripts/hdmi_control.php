25385eb21422d82e38ce8a903cac162e8c0716c8
matrix8412 <susek.martin@gmail.com>
2017-05-10 14:47:37 +0200

    Update hdmi_control.php

--- opt/scripts/hdmi_control.php
+++ opt/scripts/hdmi_control.php
@@ -6,12 +6,11 @@ $time_on = "";
 $time_off = "";
 
 $current_time = date("H:i");
-$time_on = exec("curl -sk ".$cloud_hotname."/api/api.php --data \"action=get_time_on&sn=$sn\"");
-$time_off = exec("curl -sk ".$cloud_hotname."/api/api.php --data \"action=get_time_off&sn=$sn\"");
+$hdmi_control = explode("-", exec("curl -sk ".$cloud_hotname."/api/api.php --data \"action=get_hdmi_control&sn=$sn\""));
 
-$date1 = DateTime::createFromFormat('H:i', $current_time);
-$date2 = DateTime::createFromFormat('H:i', $time_on);
-$date3 = DateTime::createFromFormat('H:i', $time_off);
+$date1 = DateTime::createFromFormat('H:i', $hdmi_control[0]);
+$date2 = DateTime::createFromFormat('H:i', $hdmi_control[1]);
+$date3 = DateTime::createFromFormat('H:i', $hdmi_control[2]);
 
 
 if($date1 > $date2 && $date1 < $date3){
