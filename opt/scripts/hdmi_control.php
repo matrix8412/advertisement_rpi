6054ed88210801e54bfa58bd3ea670d64e9e0b07
matrix8412 <susek.martin@gmail.com>
2017-08-30 23:44:31 +0200

    Update hdmi_control.php

--- opt/scripts/hdmi_control.php
+++ opt/scripts/hdmi_control.php
@@ -1,20 +1,35 @@
 <?php
 include("/opt/cron/config.php");
 
+$hdmi_state_on = "0x12000a";
+$hdmi_state_off = "0x120002";
+
 $sn = exec("cat /proc/cpuinfo | grep Serial | awk '{print $3}'");
 
-$current_time = date("H:i");
 $hdmi_control_result = exec("curl -sk ".$cloud_hostname."/api/api.php --data \"action=get_hdmi_control&sn=$sn\"");
 $hdmi_control = explode("-", $hdmi_control_result);
+$hdmi_timer = $hdmi_control[0];
+$hdmi_status = explode(" ", exec("tvservice -s"));
 
 $current_time = date("H:i:s");
 
+echo $hdmi_status[1]."\n";
 if($current_time > $hdmi_control[1] && $current_time < $hdmi_control[2]){
-  exec("/opt/vc/bin/tvservice -p");
-  echo "Turning on HDMI...\n";
+    if($hdmi_status[1] == $hdmi_state_off){
+        echo "HDMI status is OFF\n";
+        exec("/opt/vc/bin/tvservice -p");
+        echo "Turning on HDMI...\n";
+    }else{
+        echo "HDMI status is ON\n";
+    }
 }else{
-  exec("/opt/vc/bin/tvservice -o");
-  echo "Turning off HDMI...\n";
+    if($hdmi_status[1] == $hdmi_state_on){
+        echo "HDMI status is OFF\n";
+    }else{
+        echo "HDMI status is ON\n";
+        exec("/opt/vc/bin/tvservice -o");
+        echo "Turning off HDMI...\n";
+    }
 }
 
 ?>
