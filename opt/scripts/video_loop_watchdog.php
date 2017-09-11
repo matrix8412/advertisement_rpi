8d0afb85cee1ceb155a08d57a7da7376028988c5
matrix8412 <susek.martin@gmail.com>
2017-09-11 23:20:34 +0200

    Update video_loop_watchdog.php

--- opt/scripts/video_loop_watchdog.php
+++ opt/scripts/video_loop_watchdog.php
@@ -1,5 +1,13 @@
 <?php
 $pid = exec("ps axf |grep \"sudo /usr/local/bin/video_loop\" | grep -v grep | awk '{print $1}'");
+$sn = exec("cat /proc/cpuinfo | grep Serial | awk '{print $3}'");
+
+$hdmi_control_result = exec("curl -sk ".$cloud_hostname."/api/api.php --data \"action=get_hdmi_control&sn=$sn\"");
+$hdmi_control = explode("-", $hdmi_control_result);
+$hdmi_timer = $hdmi_control[0];
+$hdmi_status = explode(" ", exec("tvservice -s"));
+
+$current_time = date("H:i:s");
 
 if($pid != " " && $pid != ""){
   echo "Video loop running.\n";
